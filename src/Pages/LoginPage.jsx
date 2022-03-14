4639985fd2bc6119eb3f8fcd7df88c6847967712
Alex Tran <alextran8385@gmail.com>
2022-03-14 15:40:50 +1100

    basic html skeleton

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Loginpage = () => {
     return (
-        <div>
-            login page
+        <section className='signup'>
+    
+        <div className='signupcard'>
+        <form>
+        <h1>Login</h1>
+        <input placeholder='Email' required ></input>
+        <input placeholder='Password Minimum 6 characters' required></input>
+        <button >Log In</button>
+        </form>
+
+        <p>Don't have an account? <Link to="/signup"><a id='signin__redirect'>Create an account here</a></Link></p>
+
         </div>
+        </section>
     );
 }
 
--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signuppage = () => {
     return (
-        <div>
-            signup
+        <section className='signup'>
+    
+        <div className='signupcard'>
+        <form >
+        <h1>SignUp</h1>
+        <input placeholder='Email' required ></input>
+        <input placeholder='Password Minimum 6 characters' required></input>
+        <button >SignUp</button>
+        </form>
+
+        <p>Already Have An Account? <Link to="/login"><a id='signin__redirect'>Sign In here</a></Link></p>
+
         </div>
+        </section>
     );
 }
 
