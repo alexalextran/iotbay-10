a462e0a9116983d67f61a75f41f240e85d5ff581
alexalextran <alextran8385@gmail.com>
2022-03-22 21:40:21 +1100

    set up all the images and more styling

--- src/UI/ProductCard.jsx
+++ src/UI/ProductCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Productcard = (props) => {
+
+  
     return (
-        <div className='card'>
+        <div className='card' style={{ backgroundImage: `url(${props.src})` } }>
             <h3>{props.title}</h3>
+         
 
           <div className='card__description'>
             <p>{props.desc}</p>
