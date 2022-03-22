de32a1d696eeca1f425959147e2793571adc8316
Alex Tran <alextran8385@gmail.com>
2022-03-22 11:12:24 +1100

    added more changes to the structure of the page and some styling too

--- src/UI/ProductCard.jsx
+++ src/UI/ProductCard.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Productcard = (props) => {
     return (
-        <div className='prduct card'>
+        <div className='card'>
             <h3>{props.title}</h3>
 
-          
+          <div className='card__description'>
             <p>{props.desc}</p>
+          </div>
+           
 
         </div>
     );
